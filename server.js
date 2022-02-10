432f621b7b5016c32cd8f557b46d7dc480846bbb
livingyang <lhm.cold@163.com>
2022-02-10 11:52:58 +0800

    更新leancloud的支持

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+  port = process.env.LEANCLOUD_APP_PORT || 5000
 
 const app = express()
+
+console.log('port:', port);
 
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
